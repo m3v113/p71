3a6e202617f48f4e4c308586aead52cbdc9dee99
m3v113 <76668066+m3v113@users.noreply.github.com>
2022-03-12 13:45:49 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyDEYndgsDO_OzEfsmKU7uF8qge1Xh7Yroc",
+    authDomain: "ride-1eabf.firebaseapp.com",
+    projectId: "ride-1eabf",
+    storageBucket: "ride-1eabf.appspot.com",
+    messagingSenderId: "806820734841",
+    appId: "1:806820734841:web:ed509201ab32fe6cbb31a8"
+  };
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
